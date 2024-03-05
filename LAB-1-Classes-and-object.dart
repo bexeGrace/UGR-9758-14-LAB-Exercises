c73fdf92b40f46f155c751141875691e35929142
ChumiGrace <chumimesele@gmail.com>
2024-03-05 16:16:14 +0300

    CO Excercise 3

--- LAB-1-Classes-and-object.dart
+++ LAB-1-Classes-and-object.dart
@@ -14,6 +14,14 @@ class Car {
   }
 }
 
+// Excercise 3
+class ElectricCar extends Car {
+  int batteryLife;
+
+  ElectricCar(String brand, String model, int year, this.batteryLife)
+      : super(brand, model, year);
+}
+
 void main() {
   Car myCar = Car('Toyota', 'Corolla', 2022);
 
