8c49a7f7f2a7c2e502be19e5070fd5a322c98d61
ChumiGrace <chumimesele@gmail.com>
2024-03-05 15:10:13 +0300

    Excercise 2 commit

--- LAB-1-Variables-and-types.dart
+++ LAB-1-Variables-and-types.dart
@@ -1,4 +1,6 @@
 void main() {
+  // Excercise 1
+
   var a = 10;
   var b = 20;
   var c = 30;
@@ -6,4 +8,10 @@ void main() {
   var summ = a + b + c;
 
   print(summ);
+
+  // Excercise 2
+
+  String my_name = 'Betsegaw';
+
+  print(my_name.length);
 }
