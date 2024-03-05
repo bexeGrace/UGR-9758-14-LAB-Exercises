ee1850699c8a6669fe3120911dcac168a760e279
ChumiGrace <chumimesele@gmail.com>
2024-03-05 16:32:32 +0300

    EH Excercise 2

--- LAB-1-Exception-handling.dart
+++ LAB-1-Exception-handling.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
 void throwException() {
   throw Exception('This is an exception!');
+}
+
+// Excercise 2
+
+void readFile() {
+  File file = File('nonexistent_file.txt');
+  file.readAsStringSync();
 }
 
 void main() {
@@ -7,5 +16,13 @@ void main() {
     throwException();
   } catch (e) {
     print('Exception caught: $e');
+  }
+
+// Excercise 2
+
+  try {
+    readFile();
+  } on FileSystemException catch (e) {
+    print('File not found error caught: $e');
   }
 }
