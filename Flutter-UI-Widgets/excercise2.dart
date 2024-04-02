38ed8296ac6e299e45785bb9e7ba6ed38d5a5f3c
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2024-04-02 16:39:12 +0300

    change in 2

--- Flutter-UI-Widgets/excercise2.dart
+++ Flutter-UI-Widgets/excercise2.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Shopping Cart',
       home: ShoppingCartPage(),
     );
@@ -17,48 +15,60 @@ class MyApp extends StatelessWidget {
 }
 
 class ShoppingCartPage extends StatelessWidget {
-  const ShoppingCartPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 222, 212, 176),
       appBar: AppBar(
-        title: const Text('Cart'),
+        title: const Center(
+          child: Text(
+            'Cart',
+            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+          ),
+        ),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
+        backgroundColor: const Color.fromARGB(255, 133, 127, 107),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const Padding(
             padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(
-                  'Shopping Cart',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  'subtitle',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
+                Icon(Icons.shopping_cart),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shopping Cart',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'subtitle',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: 3,
+              itemCount:
+                  3, // Replace with the actual number of items in the cart
               itemBuilder: (context, index) {
                 return ListTile(
                   leading: CircleAvatar(
@@ -72,11 +82,17 @@ class ShoppingCartPage extends StatelessWidget {
                     ],
                   ),
                   trailing: const Column(
-                    mainAxisSize: MainAxisSize.min,
+                    mainAxisSize: MainAxisSize.max,
                     children: [
-                      Icon(Icons.add),
+                      Icon(
+                        Icons.add,
+                        size: 15,
+                      ),
                       Text('1'),
-                      Icon(Icons.remove),
+                      Icon(
+                        Icons.remove,
+                        size: 15,
+                      ),
                     ],
                   ),
                 );
@@ -84,11 +100,11 @@ class ShoppingCartPage extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(16.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
+                Text(
                   'Total: \$30.00', // Replace with actual total price
                   style: TextStyle(
                     fontSize: 20.0,
@@ -99,7 +115,7 @@ class ShoppingCartPage extends StatelessWidget {
                   onPressed: () {
                     // Proceed to checkout
                   },
-                  child: const Text('Checkout'),
+                  child: Text('Checkout'),
                 ),
               ],
             ),
