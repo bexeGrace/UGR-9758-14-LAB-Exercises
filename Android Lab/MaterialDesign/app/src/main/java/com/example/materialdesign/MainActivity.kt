f8bcbd8f2b25fe8e5d65b1efd2c30a92a105a4f6
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2024-05-20 22:32:25 +0300

    Third tutorial excercise done

--- Android Lab/MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ Android Lab/MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.border
+import android.content.res.Configuration
+
 
 
 class MainActivity : ComponentActivity() {
@@ -43,39 +46,54 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    // Add padding around our message
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            contentDescription = null,
             modifier = Modifier
-                // Set image size to 40 dp
                 .size(40.dp)
-                // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
-
-        // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
-            // Add a vertical space between the author and message texts
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     MaterialDesignTheme {
         Surface {
             MessageCard(
-                msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!")
+                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
             )
         }
     }
 }
+
+
+
